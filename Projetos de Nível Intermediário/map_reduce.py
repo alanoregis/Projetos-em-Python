1ff1a85e5548c15d3246f5718bb4b2041d026ad6
Alano <alano.120.ar@gmail.com>
2026-08-26 18:00:10 -0300

    Projeto que adiciona contagem de animais por tipo usando map e reduce

---
+++ Projetos de Nível Intermediário/map_reduce.py
@@ -0,0 +1,23 @@
+import json
+from functools import reduce
+
+f = open("aquario.json", encoding="utf8")
+data_aquario = json.load(f)
+animals = data_aquario["data"]
+
+def pick_animal_type(animal):
+    return animal["type"], 1
+
+def reducer(acc, val):
+    print(val)
+    if val[0] not in acc.keys():
+        acc[val[0]] = 0 + val[1]
+    else:
+        acc[val[0]] = acc[val[0]] + val[1]
+        print(acc)
+    return acc
+
+type_animals = list(map(pick_animal_type, animals))
+print(type_animals)
+animals_type_count = reduce(reducer, type_animals, {})
+print(animals_type_count)
